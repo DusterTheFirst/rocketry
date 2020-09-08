79be38ac19bde07b0074a995c71319bb84e45d3d
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2020-09-07 21:05:29 -0400

    mabye not rtic

--- avionics/oxide/src/main.rs
+++ avionics/oxide/src/main.rs
@@ -2,31 +2,33 @@
 #![cfg_attr(not(doc), no_main)]
 #![no_std]
 
+use teensy4_bsp::hal::gpio::GPIO;
 use bsp::{
-    hal::{ccm::{PLL1, ClockMode, uart}, iomuxc::consts::U1, uart, pwm},
+    hal::{
+        ccm::{pwm as pwm_ccm, ClockMode, PLL1},
+        iomuxc::consts::U2,
+        pwm,
+    },
+    t40::{P6, P9},
     LED,
-};
-use cortex_m::asm::wfi;
-use embedded_hal::digital::v2::OutputPin;
-use log::info;
+hal::gpio};
+use core::time::Duration;
+use embedded_hal::{digital::v2::OutputPin, Pwm};
 use panic_halt as _;
 use rtic::cyccnt::U32Ext;
 pub use teensy4_bsp as bsp;
 
 // The CYCCNT counts in clock cycles. Using the clock hz should give us a ~1 second period.
 const PERIOD: u32 = PLL1::ARM_HZ;
-const BAUD: u32 = 115_200; // 2_000_000
-const TX_FIFO_SIZE: u8 = 4;
 
 #[rtic::app(device = teensy4_bsp, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
-    struct Resources {
+    struct Resources<'a> {
         led: LED,
-        buzzer_tone: Option<u16>,
-        buzzer: pwm::Pin
+        buzzer: GPIO<P9, gpio::Output>
     }
 
-    #[init(schedule = [blink])]
+    #[init(schedule = [blink, buzz])]
     fn init(mut cx: init::Context) -> init::LateResources {
         init_delay();
 
@@ -47,56 +49,37 @@ const APP: () = {
         // Get the pins
         let pins = bsp::t40::pins(cx.device.iomuxc);
 
-        // Init the usb
-        let uarts = cx.device.uart.clock(
-            &mut cx.device.ccm.handle,
-            uart::ClockSelect::OSC,
-            uart::PrescalarSelect::DIVIDE_1,
-        );
-
-        let mut dma_channels = cx.device.dma.clock(&mut cx.device.ccm.handle);
-        let mut channel = dma_channels[7].take().unwrap();
-        // Enable interrupt generation when the DMA transfer completes
-        channel.set_interrupt_on_completion(true);
-
-        // Init UART6
-        let uart = uarts.uart6.init(pins.p1, pins.p0, BAUD).unwrap();
-
-        // Init the UART logger
-        let (tx, _) = uart.split();
-        imxrt_uart_log::dma::init(tx, channel, Default::default()).unwrap();
-
-        log::info!("Hello world!");
-
-        // Schedule the first blink.
-        cx.schedule.blink(cx.start + PERIOD.cycles()).unwrap();
-
         // Grab the led out og yhr pins
         let mut led = bsp::configure_led(pins.p13);
 
         // Start the led High
         led.set_high().unwrap();
 
-        init::LateResources { led }
+        let mut buzzer = 
+
+        // Schedule the first blink.
+        cx.schedule.blink(cx.start + PERIOD.cycles()).unwrap();
+
+        init::LateResources {
+            led,
+            buzzer
+        }
     }
 
-    #[task(binds = DMA7_DMA23)]
-    fn dma7_dma23(cx: dma7_dma23::Context) {
-        imxrt_uart_log::dma::poll();
+    fn buzz(cx: buzz::Context) {
+
     }
 
-    #[task(resources = [led], schedule = [blink])]
+    #[task(resources = [led, pwm2_handle, pwm2_sm2], schedule = [blink])]
     fn blink(cx: blink::Context) {
-        log::info!("Hello world!");
-
         cx.resources.led.toggle();
+
         // Schedule the following blink.
         cx.schedule.blink(cx.scheduled + PERIOD.cycles()).unwrap();
     }
 
     // RTIC requires that unused interrupts are declared in an extern block when
-    // using software tasks; these free interrupts will be used to dispatch the
-    // software tasks.
+    // using software tasks
     extern "C" {
         fn LPUART8();
         fn LPUART7();
